d6e4e27db7b96d80833d0b99fe46e8f3cfeffad1
Kosh <kosh7707@naver.com>
2022-10-26 02:37:46 +0900

    원형 더블연결리스트 구현 (Iterator 사용)

--- Circular_DoublyLinkedList/Circular_DoublyLinkedList.cpp
+++ Circular_DoublyLinkedList/Circular_DoublyLinkedList.cpp
@@ -18,6 +18,36 @@ private:
 };
 /* --------------------------------------------------------------- */
 
+/* CircularDoublyListIterator ------------------------------------ */
+template<class T>
+class CircularDoublyListIterator {
+public:
+    CircularDoublyListIterator(const CircularDoublyList<T>& l) : list(l) {
+        current = l.first->rlink;
+    }
+    T* Next() {
+        current = current->rlink;
+        if (current != list.first) return &current->data;
+        else return nullptr;
+    }
+    T* First() {
+        if (current != list.first) return &current->data;
+        else return nullptr;
+    }
+    bool NotNull() {
+        if (current != list.first) return true;
+        else return false;
+    }
+    bool NextNotNull() {
+        if (current->rlink != list.first) return true;
+        else return false;
+    }
+private:
+    const CircularDoublyList<T>& list;
+    DoublyListNode<T>* current;
+};
+/* --------------------------------------------------------------- */
+
 /* CircularDoublyList -------------------------------------------- */
 template <class T>
 class CircularDoublyList {
@@ -29,14 +59,36 @@ public:
         first->rlink = first;
         first->data = -1;
     }
-    void Insert(T x) {
+    void Insert(T t) {
         DoublyListNode<T>* p;
-        DoublyListNode<T>* temp = new DoublyListNode<T>();
-        p = first; temp->data = x;
-        while (!(p->rlink->data == -1 or x < p->rlink->data))
+        auto* temp = new DoublyListNode<T>();
+        p = first; temp->data = t;
+        while (!(p->rlink->data == -1 or t < p->rlink->data))
             p = p->rlink;
         temp->llink = p; temp->rlink = p->rlink;
         p->rlink->llink = temp; p->rlink = temp;
+    }
+    bool Delete(T t) {
+        auto* current = first;
+        do {
+            if (current->data == t) {
+                current->llink->rlink = current->rlink;
+                current->rlink->llink = current->llink;
+                delete current;
+                return true;
+            }
+            else current = current->rlink;
+        } while (current != first);
+        return false;
+    }
+    CircularDoublyList<T> Merge(CircularDoublyList<T> b) {
+        CircularDoublyList<T> c;
+        CircularDoublyListIterator<T> la(*this), lb(b);
+        if (la.NotNull()) c.Insert(*la.First());
+        while (la.NextNotNull()) c.Insert(*la.Next());
+        if (lb.NotNull()) c.Insert(*lb.First());
+        while (lb.NextNotNull()) c.Insert(*lb.Next());
+        return c;
     }
     friend ostream& operator << (ostream& os, CircularDoublyList<T> circularDoublyList) {
         CircularDoublyListIterator<T> li(circularDoublyList);
@@ -53,37 +105,6 @@ private:
 /* --------------------------------------------------------------- */
 
 
-/* CircularDoublyListIterator ------------------------------------ */
-template<class T>
-class CircularDoublyListIterator {
-public:
-    CircularDoublyListIterator(const CircularDoublyList<T>& l) : list(l) {
-        current = l.first->rlink;
-    }
-    T* Next() {
-        current = current->rlink;
-        if (current != list.first) return &current->data;
-        else return 0;
-    }
-    T* First() {
-        if (current != list.first) return &current->data;
-        else return 0;
-    }
-    bool NotNull() {
-        if (current != list.first) return true;
-        else return false;
-    }
-    bool NextNotNull() {
-        if (current->rlink != list.first) return true;
-        else return false;
-    }
-private:
-    const CircularDoublyList<T>& list;
-    DoublyListNode<T>* current;
-};
-/* --------------------------------------------------------------- */
-
-
 int main() {
     srand(time(nullptr));
     char select = '0';
@@ -92,41 +113,43 @@ int main() {
     CircularDoublyList<int> a, b, c;
 
     while (select != 'q' || select != 'Q') {
-        cout << endl << "Select command: a: Add to List a, b: Add to List b, "
-                        "d: Delete, m: Merge, p: PrintAll, q: exit" << endl;
+        cout << endl << "Select command\n[a]: Add to List a\n[b]: Add to List b\n"
+                        "[d]: Delete\n[m]: Merge\n[p]: PrintAll\n[q]: exit" << endl;
+        cout << ">>";
         cin >> select;
 
         switch (select) {
             case 'a':
-                cout << "List a 값 자동 추가 : " << endl;
+                cout << "Random value is inserted in List a" << endl;
                 for (int i=0; i<10; i++)
                     a.Insert(rand()%100);
                 break;
             case 'b':
-                cout << "List b 값 자동 추가 : " << endl;
+                cout << "Random value is inserted in List b" << endl;
                 for (int i=0; i<10; i++)
                     b.Insert(rand()%100);
                 break;
             case 'd':
-                cout << "List a와 b에서 삭제할 값을 입력 : " << endl;
+                cout << "Input value to delete :";
                 cin >> weight;
                 if (a.Delete(weight))
-                    cout << "List a에서" << weight << "값이 삭제됨." << endl;
+                    cout << "List a, " << weight << " is deleted." << endl;
                 else
-                    cout << "List a에" << weight << "값이 없음." << endl;
+                    cout << "List a has no value, " << weight << "." << endl;
                 if (b.Delete(weight))
-                    cout << "List b에서" << weight << "값이 삭제됨." << endl;
+                    cout << "List b, " << weight << " is deleted." << endl;
                 else
-                    cout << "List b에" << weight << "값이 없음." << endl;
+                    cout << "List b has no value, " << weight << "." << endl;
                 break;
             case 'm':
+                cout << "--- a.Merge(b) ---" << endl;
                 c = a.Merge(b);
                 cout << c;
                 break;
             case 'p':
-                cout << "List a의 출력: " << endl;
+                cout << "--- List a ---" << endl;
                 cout << a << endl;
-                cout << "List b의 출력: " << endl;
+                cout << "--- List b ---" << endl;
                 cout << b << endl;
                 break;
             case 'q':
